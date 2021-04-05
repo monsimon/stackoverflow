f14076e9722f6dbca7655180d8d7dc5b1b14bcc9
Samuel K <samuelkantsimo@gmail.com>
2021-04-05 19:16:51 +0700

    Salary_ML.py

--- Salary_ML.py
+++ Salary_ML.py
@@ -21,7 +21,7 @@ def clean_data(df):
     3. Create y as the Salary column
     4. Drop the Salary, Respondent, and the ExpectedSalary columns
     5. For each numeric variable, fill the column with the mean value.
-    6. Create dummy columns for all the categorical variables, drop the original columns
+    6. Create dummy columns for all the categorical variables, drop the existing original columns
     '''
     # Drop rows with missing salary values
     df = df.dropna(subset=['Salary'], axis=0)
